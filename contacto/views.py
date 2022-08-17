e97198cb6f28f15b0b430600dad8bbe6c08c04ef
Marco <marcoabustam@gmail.com>
2022-08-17 09:35:38 -0400

    variados cambios

--- contacto/views.py
+++ contacto/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import FormularioContacto
 #from contacto.models import Contacto
 
 # Create your views here.
 def contacto(request):
-    return render(request,"contacto/contacto.html")
+    formulario_contacto =FormularioContacto()
+
+    if request.method=="POST":
+        formulario_contacto=FormularioContacto(data=request.POST)
+        if formulario_contacto.is_valid():
+            nombre=request.POST.get("nombre")
+            email=request.POST.get("email")
+            contenido=request.POST.get("contenido")
+            return redirect("/contactos/?valido")
+    return render(request,"contacto/contacto.html", {"miFormulario":formulario_contacto})
---
+++ pjwebarg/static/pjwebarg/vendor/jquery/swal.js
@@ -0,0 +1,9 @@
+const showAlert = () => {
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Su información ha sido enviada con éxito',
+        showConfirmButton: false,
+        timer: 1500
+    })
+}
